4ea538413cad8e1ed56f9e6f62aff6a2a68d3cf4
mikeyolang <brunomike965@gmail.com>
2026-05-13 10:13:35 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tanstackRouterVite } from "@tanstack/router-plugin/vite";
-import { tanstackStartVite } from "@tanstack/react-start/vite";
+import { defineConfig } from "@tanstack/react-start/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tanstackRouterVite(),
-    tanstackStartVite({
-      deployment: {
-        preset: "netlify",
-      },
-    }),
-    react(),
-    tailwindcss(),
-    tsconfigPaths(),
-  ],
+  deployment: {
+    preset: "netlify",
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+      tsconfigPaths(),
+    ],
+  },
 });
