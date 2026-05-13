f43fe04e35dd6f67bc03891dafed303dbc2575f4
mikeyolang <brunomike965@gmail.com>
2026-05-13 10:29:16 +0300

    Fix again

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { tanstackRouterVite } from "@tanstack/router-plugin/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    tanstackRouterVite(),
+    TanStackRouterVite(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
